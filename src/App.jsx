93904c5f500c290899cbe3b7df3b73aba52fe710
julidas <juliandascenzo@gmail.com>
2022-07-26 12:59:25 -0300

    repo1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="nav1">
+     <ul>
+      <li>Inicio</li>
+      <li>Nosotros</li>
+      <li>Productos</li>
+     </ul>
+    </div>
+  )
+}
+
+export default App
