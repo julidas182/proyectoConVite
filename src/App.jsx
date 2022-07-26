59d5960e66316d72912a6eb6abb4994b6ed59ff0
julidas <juliandascenzo@gmail.com>
2022-07-26 17:29:51 -0300

    creandoNavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import NavBar from "./componentes/navBar/NavBar"
 
 function App() {
   
 
   return (
-    <div className="nav1">
-     <ul>
-      <li>Inicio</li>
-      <li>Nosotros</li>
-      <li>Productos</li>
-     </ul>
-    </div>
+    <>
+      <NavBar/>
+    </>
   )
 }
 
